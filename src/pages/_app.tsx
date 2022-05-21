fbbc90458c90526d6be5307a9f91592a90cc14ca
caionobrex <marialoucos@hotmail.com.br>
2022-05-21 13:27:28 -0300

    feat: animatePresence added to root of components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AnimatePresence } from 'framer-motion'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
+  )
 }
 
 export default MyApp
