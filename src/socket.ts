4fd7dcc01446cf30833addc7eb2f2493350f47c2
caionobrex <marialoucos@hotmail.com.br>
2022-11-22 19:01:37 -0300

    add comment

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,8 @@ import { io, Socket } from 'socket.io-client'
 
 let socket: Socket | null = null
 
+// test
+
 export default function getSocket(token: string | null): Socket | null {
   if (socket) return socket
   socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || '', {
